39dce02172399b72905332da3d1715d3266d3f7e
George Michaelides <g.mich90@gmail.com>
2016-02-25 14:13:54 +0200

    Added ClientManager to the chat samples.

---
+++ Samples/Chat/Ginet.Chat.Client/ClientManager.cs
@@ -0,0 +1,41 @@
+﻿using Ginet.Chat.Packages;
+using Ginet.NetPackages;
+using Lidgren.Network;
+using System;
+using System.Reflection;
+
+namespace Ginet.Chat.Client
+{
+    internal class ClientManager
+    {
+        public readonly NetworkClient client;
+        public IPackageSender DefaultPackageSender { get; }
+        public IncomingMessageHandler MessageHandler => client.IncomingMessageHandler;
+
+        public ClientManager(Action<NetPeerConfiguration> configuration)
+        {
+             client = new NetworkClient("Chat",
+             container =>
+                 container.RegisterPackages(Assembly.Load("Ginet.Chat.Packages")),
+             configuration);
+
+            client.Start(NetDeliveryMethod.UnreliableSequenced, 0);
+            client.ProcessMessagesInBackground();
+
+            DefaultPackageSender = client.LiftSender((msg, peer) =>
+                peer.SendMessage(msg, NetDeliveryMethod.ReliableOrdered));
+        }
+
+        public void ConnectClient(ConnectionApprovalMessage message)
+        {
+            client.Connect("localhost", 1234, message);
+        }
+
+        public void Disconnect()
+        {
+            client.Stop("bb");
+        }
+    }
+
+}
+
